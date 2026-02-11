3a987c27dcbaa23bd634689dbb4750d07c1de01a
amir-mansoor <qazimansor89@gmail.com>
2026-02-11 20:20:24 +0500

    lecture 1- datatypes, functions prototypes

---
+++ lecture-1/datatypes_functions_prototypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+// function declaration #parameters #return type
+
+/* 
+endl --- New line
+*/
+int main() {
+    cout << "Hello world" << endl;
+    return 0;
+}
