da1d16d34f6bd8e31ff9ffa22ae5c305a0c7889e
amir-mansoor <qazimansor89@gmail.com>
2026-05-17 17:03:50 +0500

    linked list completed

--- 6-lists-with-pointers-part2/linked-list.cpp
+++ 6-lists-with-pointers-part2/linked-list.cpp
@@ -6,6 +6,12 @@ struct node {
     node *next;
 };
 
+void delete_node(node *current) {
+    node *temp = current->next;
+    current->next = current->next->next;
+    delete temp;
+}
+
 node* create_node(node *curr, int val) {
     node *n = new node;
     n->val = val;
@@ -27,6 +33,19 @@ void output_list(node *head) {
 
 }
 
+
+void sum_list(node *head) {
+    node *curr = head;
+    int sum = 0;
+    while(curr != NULL) {
+        sum += curr->val;
+        curr = curr->next;
+
+    }
+    cout << "Sum is: " << sum << endl;
+
+}
+
 int main(){
     node *head,*curr;
     head->val = 0;
@@ -36,6 +55,11 @@ int main(){
         curr = create_node(curr, i);
     }
 
+    output_list(head);
+    curr = head->next;
+    sum_list(head);
+    cout << "Going to delete: " << curr->val << endl;
+    delete_node(head);
     output_list(head);
     return 0;
 }
